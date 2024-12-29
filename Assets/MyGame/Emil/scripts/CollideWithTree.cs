28e814664de630616b62c942039ecd3c0e007aee
eoberndorfer08 <emil.oberndorfer08@htl-salzburg.ac.at>
2024-12-29 10:04:58 +0100

    win screen updated and tree counter added

--- Assets/MyGame/Emil/scripts/CollideWithTree.cs
+++ Assets/MyGame/Emil/scripts/CollideWithTree.cs
@@ -1,18 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CollideWithTree : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public TextMeshProUGUI treeCounterText; // TMP Text zur Anzeige der Baumanzahl
+    public GameObject winTextPrefab; // Prefab für den Gewonnen-Text
+    public Transform canvasTransform; // Der Canvas, auf dem der Text erscheint
+    private int treeCount;
+
     void Start()
     {
-        
+        UpdateTreeCount(); // Initialisiere die Baumanzahl
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnCollisionEnter(Collision collision)
     {
-        
+        // Überprüfen, ob das Objekt mit dem Baum kollidiert
+        if (collision.gameObject.CompareTag("Tree"))
+        {
+            Destroy(collision.gameObject); // Zerstöre den Baum
+            StartCoroutine(DelayedTreeCountUpdate()); // Verzögerte Aktualisierung der Baumanzahl
+        }
     }
+
+    private IEnumerator DelayedTreeCountUpdate()
+    {
+        yield return new WaitForEndOfFrame(); // Warte bis zum Ende des aktuellen Frames
+        UpdateTreeCount(); // Aktualisiere die Baumanzahl
+    }
+
+    void UpdateTreeCount()
+    {
+        treeCount = GameObject.FindGameObjectsWithTag("Tree").Length; // Aktualisiere den Baumzähler
+        treeCounterText.text = "Trees Left: " + treeCount; // Aktualisiere den Text
+
+        if (treeCount == 0)
+        {
+            SceneManager.LoadScene("winscene");
+        }
+    }
+
+  
 }
+
